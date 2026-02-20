e99cc0c095b58bc56520131e793d7301275f17be
Amnesia-NL <wakzzedit2@gmail.com>
2026-02-20 03:56:53 +0100

    Source code

---
+++ NPCAction/Main.cs
@@ -0,0 +1,23 @@
+﻿using AmnesiaTools;
+using AmnesiaTools.Core;
+using Life;
+using Life.Network;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace NPCAction
+{
+    public class NPCAction : AmnesiaSystem
+    {
+        PluginInformations pluginInformation;
+        public NPCAction(IGameAPI aPI) : base(aPI)
+        {
+            pluginInformation = new PluginInformations("NPCAction", "V.1.0.0", "Zerox");
+        }
+        public override async void OnPlayerInput(Player player, KeyCode keyCode, bool onUI)
+        {
+            base.OnPlayerInput(player, keyCode, onUI);
+            if (keyCode == KeyCode.F8 && !onUI) await NPCUI.NPCPanel(player); 
+        }
+    }
+}
---
+++ NPCAction/NPCUI.cs
@@ -0,0 +1,36 @@
+﻿using AmnesiaTools.Core.UI.UIHelper;
+using AmnesiaTools.Core.Utils;
+using AmnesiaTools.Core.Utils.Entity;
+using CMF;
+using Life.Network;
+using Life.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace NPCAction
+{
+    public class NPCUI
+    {
+        public static async Task NPCPanel(Player player)
+        {
+            PanelUI panel = new PanelUI("PNJ", UIPanel.PanelType.TabPrice, player, () => NPCPanel(player));
+            foreach(Collider collider in Physics.OverlapSphere(player.setup.transform.position,5f))
+            {
+                if(collider.gameObject.CompareTag("Amnesia_NPC"))
+                {
+                    List<NPC> npcs = await NPC.Query(x => Vector3.Distance(x.Position, collider.transform.position) < 0.1f);
+                    if (!npcs.Any()) continue;
+                    NPC npc = npcs.FirstOrDefault();
+                    panel.AddTabLine(Text.Gradient.ApplyGrandient(npc.Name, Color.red, Color.cyan), ui => { panel.PanelClose(); npc.Action?.Invoke(); });
+                }
+            }
+            panel.CloseButtonColor();
+            panel.SelectButtonNameColor("Parler");
+            panel.ShowPanel();
+        }
+    }
+}
