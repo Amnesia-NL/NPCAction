78d76878521c52cae45c26f584e84022dcd599b6
Amnesia-NL <wakzzedit2@gmail.com>
2026-02-20 04:11:40 +0100

    Icon

--- NPCAction/NPCUI.cs
+++ NPCAction/NPCUI.cs
@@ -17,7 +17,7 @@ namespace NPCAction
     {
         public static async Task NPCPanel(Player player)
         {
-            PanelUI panel = new PanelUI("PNJ", UIPanel.PanelType.TabPrice, player, () => NPCPanel(player));
+            PanelUI panel = new PanelUI("PNJ", UIPanel.PanelType.TabPrice, player, async () => await NPCPanel(player));
             foreach(Collider collider in Physics.OverlapSphere(player.setup.transform.position,5f))
             {
                 if(collider.gameObject.CompareTag("Amnesia_NPC"))
@@ -25,7 +25,7 @@ namespace NPCAction
                     List<NPC> npcs = await NPC.Query(x => Vector3.Distance(x.Position, collider.transform.position) < 0.1f);
                     if (!npcs.Any()) continue;
                     NPC npc = npcs.FirstOrDefault();
-                    panel.AddTabLine(Text.Gradient.ApplyGrandient(npc.Name, Color.red, Color.cyan), ui => { panel.PanelClose(); npc.Action?.Invoke(); });
+                    panel.AddTabLine(Text.ApplyAlign(Text.Gradient.ApplyGrandient(npc.Name, Color.red, Color.cyan),Text.Align.Center),"",PlayerUtils.Items.GetItemIconById(1127), ui => { panel.PanelClose(); npc.Action?.Invoke(); });
                 }
             }
             panel.CloseButtonColor();
